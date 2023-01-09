759641bf25111422541b6e31e362a451455ac3c7
Nithin BS <83852776+nithin2027@users.noreply.github.com>
2023-01-09 10:37:32 +0530

    Create Lab8.cpp

---
+++ Lab8.cpp
@@ -0,0 +1,91 @@
+#include<iostream>
+#include<cstdlib>
+#include<string>
+#include<cstdio>
+using namespace std;
+const int T_S = 200;
+class HashTableEntry {
+   public:
+      int k;
+      int v;
+      HashTableEntry(int k, int v) {
+         this->k= k;
+         this->v = v;
+      }
+};
+class HashMapTable {
+   private:
+      HashTableEntry **t;
+   public:
+      HashMapTable() {
+         t = new HashTableEntry * [T_S];
+         for (int i = 0; i< T_S; i++) {
+            t[i] = NULL;
+         }
+      }
+      int HashFunc(int k) {
+         return k % T_S;
+      }
+      void Insert(int k, int v) {
+         int h = HashFunc(k);
+         while (t[h] != NULL && t[h]->k != k) {
+            h = HashFunc(h + 1);
+         }
+         if (t[h] != NULL)
+            delete t[h];
+         t[h] = new HashTableEntry(k, v);
+      }
+      int SearchKey(int k) {
+         int h = HashFunc(k);
+         while (t[h] != NULL && t[h]->k != k) {
+            h = HashFunc(h + 1);
+         }
+         if (t[h] == NULL)
+            return -1;
+         else
+            return t[h]->v;
+      }
+
+      ~HashMapTable() {
+         for (int i = 0; i < T_S; i++) {
+            if (t[i] != NULL)
+               delete t[i];
+               delete[] t;
+         }
+      }
+};
+int main() {
+   HashMapTable hash;
+   int k, v;
+   int c;
+   while (1) {
+      cout<<"1.Insert"<<endl;
+      cout<<"2.Search"<<endl;
+      cout<<"Enter your choice: ";
+      cin>>c;
+      switch(c) {
+         case 1:
+            cout<<"Enter element to be inserted: ";
+            cin>>v;
+            cout<<"Enter key at which element to be inserted: ";
+            cin>>k;
+            hash.Insert(k, v);
+         break;
+         case 2:
+            cout<<"Enter key of the element to be searched: ";
+            cin>>k;
+            if (hash.SearchKey(k) == -1) {
+               cout<<"No element found at key "<<k<<endl;
+               continue;
+            } else {
+               cout<<"Element at key "<<k<<" : ";
+               cout<<hash.SearchKey(k)<<endl;
+            }
+         break;
+
+         default:
+            cout<<"\nEnter correct option\n";
+      }
+   }
+   return 0;
+}
